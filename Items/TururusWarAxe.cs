7f935fd6be05b5c79ea120c6d69ef2018b6d5186
Daniel Pérez Valverde <dpv927@inlumine.ual.es>
2024-02-05 00:01:43 +0100

    War axe

---
+++ Items/TururusWarAxe.cs
@@ -0,0 +1,35 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TururusMod.Items {
+
+	public class TururusWarAxe : ModItem {
+
+		public override void SetDefaults() {
+			Item.damage = 2800;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 61;
+			Item.height = 90;
+			Item.useTime = 8;
+			Item.useAnimation = 20;
+			Item.useStyle = 1;
+			Item.knockBack = 18;
+			Item.value = 1;
+			Item.rare = -12;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+		}
+
+		public override void AddRecipes() {
+            // 703 704 705 706
+			Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.TinBar, 2);
+            recipe.AddIngredient(ItemID.LeadBar, 2);
+            recipe.AddIngredient(ItemID.TungstenBar, 2);
+            recipe.AddIngredient(ItemID.PlatinumBar, 2);        
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.Register();
+		}
+	}
+}
--- README.md
+++ README.md
@@ -4,8 +4,9 @@
 
 | Aspecto | Nombre |
 | ------- | ------ |
-| <img src='Items/TururusAxe.png'>   | Tururu's Axe   |
-| <img src='Items/TururusSword.png'> | Tururu's Sword |
+| <img src='Items/TururusAxe.png'>    | Tururu's Axe       |
+| <img src='Items/TururusSword.png'>  | Tururu's Sword     |
+| <img src='Items/TururusWarAxe.png'> | Tururu's War Axe   |
 
 # NPCs
 
