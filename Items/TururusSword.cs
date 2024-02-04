4d2b5aeab843ff3de623e4a42f21eea027497748
Daniel Pérez Valverde <dpv927@inlumine.ual.es>
2024-02-04 17:06:43 +0100

    Add sword and axe

---
+++ Items/TururusSword.cs
@@ -0,0 +1,35 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TururusMod.Items {
+
+	public class TururusSword : ModItem {
+
+		public override void SetDefaults() {
+			Item.damage = 3000;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 31;
+			Item.height = 107;
+			Item.useTime = 10;
+			Item.useAnimation = 20;
+			Item.useStyle = 1;
+			Item.knockBack = 30;
+			Item.value = 1;
+			Item.rare = -12;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+		}
+
+		public override void AddRecipes() {
+            // 703 704 705 706
+			Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.TinBar, 2);
+            recipe.AddIngredient(ItemID.LeadBar, 2);
+            recipe.AddIngredient(ItemID.TungstenBar, 2);
+            recipe.AddIngredient(ItemID.PlatinumBar, 2);        
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.Register();
+		}
+	}
+}
