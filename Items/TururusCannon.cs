9de6c97198036e4649e6f54ba5ddd3b73cc921d8
Daniel Pérez Valverde <dpv927@inlumine.ual.es>
2024-02-05 14:06:20 +0100

    Tururus cannon

--- Items/TururusCannon.cs
+++ Items/TururusCannon.cs
@@ -1,28 +1,30 @@
-using Terraria;
+using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria;
+using Microsoft.Xna.Framework;
 
 namespace TururusMod.Items {
 
 	public class TururusCannon : ModItem {
 
 		public override void SetDefaults() {
-			Item.damage = 550;
-			Item.DamageType = DamageClass.Magic;
+			Item.damage = 650;
+			Item.DamageType = DamageClass.Ranged;	
 			Item.width = 59;
 			Item.height = 83;
-			Item.useTime = 5;
+			Item.useTime = 10;
 			Item.useAnimation = 20;
-			Item.useStyle = 1;
-			Item.knockBack = 15;
+			Item.useStyle = 5;
+			Item.knockBack = 2;
 			Item.value = 1;
 			Item.rare = -12;
+			Item.UseSound = SoundID.Item11;
+			Item.autoReuse = true;
 			Item.shoot = 1;
 			Item.useAmmo = AmmoID.Bullet;
-			Item.UseSound = SoundID.Item1;
-			Item.shootSpeed = 10.5f;
+			Item.shootSpeed = 30f;
 			Item.noMelee = true;
-			Item.autoReuse = true;
 		}
 
 		public override void AddRecipes() {
@@ -34,6 +36,17 @@ namespace TururusMod.Items {
             recipe.AddIngredient(ItemID.PlatinumBar, 2);        
 			recipe.AddTile(TileID.WorkBenches);
 			recipe.Register();
+		}
+
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback) {
+			Vector2 offset = new Vector2(velocity.X * 3, velocity.Y * 3);
+			position += offset;
+			return true;
+        }
+
+		public override Vector2? HoldoutOffset() {
+			Vector2 offset = new Vector2(6, 0);
+			return offset;
 		}
 	}
 }
