daaed386287ce99471d67cabbb9d92829ad28071
Daniel Pérez Valverde <dpv927@inlumine.ual.es>
2024-02-10 15:30:43 +0100

    Tururus Genesis

--- obj/Debug/net6.0/TururusMod.AssemblyInfo.cs
+++ obj/Debug/net6.0/TururusMod.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TururusMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da2ca59919801c3f06649c544a0609a3a5e8aa4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3b00683afdd4d90f3c2fc1434c524a2089bc582")]
 [assembly: System.Reflection.AssemblyProductAttribute("TururusMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TururusMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
