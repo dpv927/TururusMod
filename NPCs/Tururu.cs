4b47f865fdd84c348462c713f3e7a2b128a1603c
Daniel Pérez Valverde <dpv927@inlumine.ual.es>
2024-02-13 22:55:04 +0100

    Update tururu sprites

--- NPCs/Tururu.cs
+++ NPCs/Tururu.cs
@@ -1,143 +1,113 @@
-﻿using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using Terraria;
-using Terraria.GameContent;
-using Terraria.GameContent.Bestiary;
 using Terraria.ID;
 using Terraria.ModLoader;
 using TururusMod.Items.Ammo;
 using TururusMod.Items.Weapons.Magic;
 using TururusMod.Items.Weapons.Melee;
 using TururusMod.Items.Weapons.Ranged;
-using TururusMod.Projectiles.Ranged;
 
 namespace TururusMod.NPCs {
 
     [AutoloadHead]
     public class Tururu : ModNPC {
 
-        public const string ShopName = "Shop";
-        private static Profiles.StackedNPCProfile NPCProfile;
-
-        public override void SetStaticDefaults() {
-            NPC.width = 20;
+        public override void SetDefaults() {
+            NPC.townNPC = true;
+            NPC.friendly = true;
             NPC.height = 20;
+            NPC.width = 20;
             NPC.aiStyle = 7;
             NPC.defense = 35;
-            NPC.lifeMax = 300;
+            NPC.lifeMax = 900;
             NPC.HitSound = SoundID.NPCHit1;
             NPC.DeathSound = SoundID.NPCDeath1;
-            NPC.knockBackResist = 0.5f;
+            NPC.knockBackResist = 1.5f;
+            NPCID.Sets.NoTownNPCHappiness[NPC.type] = false;
             Main.npcFrameCount[NPC.type] = 25;
-            NPCID.Sets.HatOffsetY[Type] = 4; 
             NPCID.Sets.ExtraFramesCount[NPC.type] = 0;
             NPCID.Sets.AttackFrameCount[NPC.type] = 1;
             NPCID.Sets.DangerDetectRange[NPC.type] = 500;
             NPCID.Sets.AttackType[NPC.type] = 1;
-            NPCID.Sets.AttackTime[NPC.type] = 30;
+            NPCID.Sets.AttackTime[NPC.type] = 20;
             NPCID.Sets.AttackAverageChance[NPC.type] = 10;
-            NPCID.Sets.NoTownNPCHappiness[Type] = true;
-            NPCID.Sets.ActsLikeTownNPC[Type] = true;
+            NPCID.Sets.HatOffsetY[NPC.type] = 4;
             AnimationType = 22;
         }
 
-        public override void SetDefaults() {
-            NPC.townNPC = true;
-            NPC.friendly = true;
-            NPC.width = 20;
-            NPC.height = 20;
-            NPC.aiStyle = 7;
-            NPC.defense = 35;
-            NPC.lifeMax = 300;
-            NPC.HitSound = SoundID.NPCHit1;
-            NPC.DeathSound = SoundID.NPCDeath1;
-            NPC.knockBackResist = 0.5f;
-            AnimationType = NPCID.Guide;
-        }
-
         public override bool CanTownNPCSpawn(int numTownNPCs) {
-            return true;
-        }
-
-        public override bool CanChat() {
-            return true;
-        }
-
-        public override bool UsesPartyHat() {
+            for (int i = 0; i<255 ;i++) {
+                Player player = Main.player[i];
+                
+                foreach(Item item in player.inventory) {
+                    if(item.type == ItemID.WoodenArrow) {
+                        return true;
+                    }
+                }
+            }            
             return false;
         }
 
-        public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry) {
-            bestiaryEntry.Info.AddRange(new IBestiaryInfoElement[] {
-				new FlavorTextBestiaryInfoElement("I can't believe it! It's the real Tururu in person!"),
-            });
-        }
-
-        public override ITownNPCProfile TownNPCProfile() {
-            return NPCProfile;
-        }
-
         public override List<string> SetNPCNameList() {
-            return new List<string> {
-                "Tururu",
-            };
+            return new List<string> { "Tururu" };
         }
 
         public override void SetChatButtons(ref string button, ref string button2) {
             button = "Shop";
         }
 
-        public override void OnChatButtonClicked(bool firstButton, ref string shop) {
-            if (firstButton)
-                shop = "Shop";
+        public override void OnChatButtonClicked(bool firstButton, ref string shopName) {
+            if (firstButton) shopName = "Shop";
         }
 
         public override void AddShops() {
-            new NPCShop(Type)
-                .Add<TururusGenesis>()
+            var shop = new NPCShop(Type, "Shop")
                 .Add<TururusSword>()
-                .Add<TururusYoyo>()
                 .Add<TururusCannon>()
+                .Add<TururusGenesis>()
                 .Add<CannonBullet>()
-                .Register();
+                .Add<TururusYoyo>();
+            shop.Register();
         }
 
         public override string GetChat() {
             NPC.FindFirstNPC(ModContent.NPCType<Tururu>());
-            switch (Main.rand.Next(5)) {
-                case 0: return "Pa que luego digan, no?";
-                case 1: return "Mas te vale.";
-                case 2: return "Vete que hueles a filete.";
-                case 3: return "Referencia a DS1.";
-                case 4: return "Dile a 'Paco' que encienda el Fornais.";
-                default: return "Eing??";
-            }
+            var options = new List<string> {
+                "Pa que luego digan, no?",
+                "Dile al tal 'Paco' que encienda el Fornais.",
+                "Vete que hueles a filete.",
+                "Persona 5 Dancing in the Moonlight Goty of the year del año",
+                "Menuda cochinada"
+            };
+            return options[Main.rand.Next(options.Count)];
+        }
+
+        public override void TownNPCAttackStrength(ref int damage, ref float knockback) {
+            damage = 15;
+            knockback = 2f;
+        }
+
+        public override void TownNPCAttackCooldown(ref int cooldown, ref int randExtraCooldown) {
+            cooldown = 5;
+            randExtraCooldown = 10;
         }
 
         public override void TownNPCAttackProj(ref int projType, ref int attackDelay) {
-            projType = ModContent.ProjectileType<TururusCannonProjectile>();
+            projType = ProjectileID.Bullet;
             attackDelay = 1;
-
-            if (NPC.localAI[3] > attackDelay) {
-                attackDelay = 12;
-            } if (NPC.localAI[3] > attackDelay) {
-                attackDelay = 24;
-            }
         }
 
-        public override void DrawTownAttackGun(ref Texture2D item, ref Rectangle itemFrame, ref float scale, ref int horizontalHoldoutOffset) {
-            if (!NPC.IsShimmerVariant) {
-                int itemType = ModContent.ItemType<TururusCannon>();
-                Main.GetItemDrawFrame(itemType, out item, out itemFrame);
-                horizontalHoldoutOffset = (int)Main.DrawPlayerItemPos(1f, itemType).X - 12;
-            } 
-            else {
-                item = ModContent.Request<Texture2D>(Texture + "_Shimmer_Gun").Value;
-                itemFrame = item.Frame();
-                horizontalHoldoutOffset = -2;
-            }
+        public override void TownNPCAttackProjSpeed(ref float multiplier, ref float gravityCorrection, ref float randomOffset) { 
+            multiplier = 7f;
+        }
+
+        public override void OnKill() {
+            Item.NewItem(
+                NPC.GetSource_Death(),
+                NPC.getRect(),
+                ModContent.ItemType<CannonBullet>(),
+                25, false, 0, false, false
+            );
         }
     }
 }
