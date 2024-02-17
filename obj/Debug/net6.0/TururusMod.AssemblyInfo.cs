310704783ff598f947b69c40657b4b1a66b6e08c
Daniel Pérez Valverde <dpv927@inlumine.ual.es>
2024-02-17 01:15:29 +0100

    Update NPC sprites

--- obj/Debug/net6.0/TururusMod.AssemblyInfo.cs
+++ obj/Debug/net6.0/TururusMod.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TururusMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d3921d63857bbdf461f2db0bccaed3aba782e51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b47f865fdd84c348462c713f3e7a2b128a1603c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TururusMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TururusMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
