dcf6d6a7ec8cd541ededfb969b42e03f1bf133c4
Daniel Pérez Valverde <dpv927@inlumine.ual.es>
2024-02-24 01:40:43 +0100

    update

--- obj/Debug/net6.0/TururusMod.AssemblyInfo.cs
+++ obj/Debug/net6.0/TururusMod.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TururusMod")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c4d6d01b9fb59ed4b37b62fa53c411b7ebbe577")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8293f0a9bfbb2de9fcf19242f3f6494253a4c644")]
 [assembly: System.Reflection.AssemblyProductAttribute("TururusMod")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TururusMod")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
